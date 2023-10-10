df5a06edf48941393fb7d46ab1294aebf8985e1b
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2023-10-10 09:40:52 +0100

    set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MantineProvider } from "@mantine/core";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <ToastContainer />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </QueryClientProvider>
+  );
 }
